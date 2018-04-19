c9f711f1b3adce5edfc33033271d9388d28bb0ec
caraclarke <cara.clarke8@gmail.com>
2018-04-19 15:47:46 -0400

    youtube api key

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const container = document.querySelector(".container");
 
+const API_KEY = "AIzaSyCoDnKsJuO67LLV9TBOFgE9cBT_faji3ow";
+
 const App = () => {
   return <div>hi</div>
 }
